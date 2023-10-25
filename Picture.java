3a4a185d67b3271d96a8911fab4e66b43d5d05af
Octavio <octaviorojasbarredo@gmail.com>
2023-10-25 13:57:02 +0200

    Añadida luna de color magenta

--- Picture.java
+++ Picture.java
@@ -15,7 +15,8 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
-
+    private Circle moon;
+    
     /**
      * Constructor for objects of class Picture
      */
@@ -54,6 +55,12 @@ public class Picture
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
+        
+        moon = new Circle();
+        moon.makeVisible();
+        moon.moveHorizontal(-100);
+        moon.changeSize(25);
+        moon.changeColor("magenta");
     }
 
     /**
